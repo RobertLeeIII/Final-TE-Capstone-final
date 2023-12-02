ab912162dadc97057486ae2fbb31e6e5cfabb116
Ted Nelson <theodorenelson@gmail.com>
2023-12-02 18:00:05 -0500

    SQL Build script updated. Possibly too many tables, but we don't have to use all of them

--- dotnet/database/Potluck Planner Build Script.sql
+++ dotnet/database/Potluck Planner Build Script.sql
@@ -30,8 +30,8 @@ CREATE TABLE users (
 );
 
 CREATE TABLE potlucks (
-    potluck_ID INT NOT NULL IDENTITY(1001,1) PRIMARY KEY,
-    host_ID INT NOT NULL FOREIGN KEY REFERENCES users(user_id),
+    potluck_id INT NOT NULL IDENTITY(1001,1) PRIMARY KEY,
+    host_id INT NOT NULL FOREIGN KEY references users(user_id),
     potluck_name NVARCHAR(100) NOT NULL,
 	summary NVARCHAR(MAX) NULL,
     location NVARCHAR(100) NOT NULL,
@@ -42,14 +42,10 @@ CREATE TABLE potlucks (
 );
 
 CREATE TABLE dishes (
-    dish_ID INT NOT NULL IDENTITY(1501,1) PRIMARY KEY,
+    dish_id INT NOT NULL IDENTITY(1501,1) PRIMARY KEY,
     dish_name NVARCHAR(50) NOT NULL,
-    recipe NVARCHAR(MAX) NOT NULL
-);
-
-CREATE TABLE titles (
-    title_id INT NOT NULL IDENTITY(401,1) PRIMARY KEY,
-    title_name NVARCHAR(20) NOT NULL
+    recipe NVARCHAR(MAX) NOT NULL,
+	rating numeric(2,1) NULL
 );
 
 CREATE TABLE ingredients (
@@ -57,55 +53,103 @@ CREATE TABLE ingredients (
     ingredient_name NVARCHAR(50) NOT NULL
 );
 
+CREATE TABLE diets (
+	diet_id INT NOT NULL IDENTITY(601,1) PRIMARY KEY,
+	diet_name NVARCHAR(30)
+);
+
+CREATE TABLE titles (
+    title_id INT NOT NULL IDENTITY(401,1) PRIMARY KEY,
+    title_name NVARCHAR(20) NOT NULL
+);
+
+CREATE TABLE courses (
+	course_id INT IDENTITY(1,1) PRIMARY KEY,
+	course_name NVARCHAR(25) NOT NULL
+);
+
 CREATE TABLE user_title (
-    user_id INT NOT NULL,
-    title_id INT NOT NULL,
-    PRIMARY KEY (user_id, title_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (title_id) REFERENCES titles(title_id)
+    user_id INT FOREIGN KEY references users(user_id),
+    title_id INT FOREIGN KEY references titles(title_id),
+    PRIMARY KEY (user_id, title_id)
 );
 
 CREATE TABLE user_dish (
-    user_id INT NOT NULL,
-    dish_id INT NOT NULL,
+    user_id INT FOREIGN KEY references users(user_id),
+    dish_id INT FOREIGN KEY references dishes(dish_id),
     PRIMARY KEY (user_id, dish_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (dish_id) REFERENCES dishes(dish_id)
 );
 
 CREATE TABLE potluck_dish (
-    potluck_id INT NOT NULL,
-    dish_id INT NOT NULL,
-    PRIMARY KEY (potluck_id, dish_id),
-    FOREIGN KEY (potluck_id) REFERENCES potlucks(potluck_id),
-    FOREIGN KEY (dish_id) REFERENCES dishes(dish_id)
+    potluck_id INT FOREIGN KEY references potlucks(potluck_id),
+    dish_id INT FOREIGN KEY references dishes(dish_id),
+    PRIMARY KEY (potluck_id, dish_id)
 );
 
 CREATE TABLE potluck_user (
-    potluck_id INT NOT NULL,
-    user_id INT NOT NULL,
-	status NVARCHAR(20) NOT NULL DEFAULT 'Invited', --Attending, Not Attenging
-    PRIMARY KEY (potluck_id, user_id),
-    FOREIGN KEY (potluck_id) REFERENCES potlucks(potluck_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    potluck_id INT FOREIGN KEY references potlucks(potluck_id),
+    user_id INT FOREIGN KEY references users(user_id),
+	status NVARCHAR(20) NOT NULL DEFAULT 'Invited', --Attending, Not Attenging, etc.
+    PRIMARY KEY (potluck_id, user_id)
+    
 );
 
 CREATE TABLE ingredient_dish (
-    ingredient_id INT NOT NULL,
-    dish_id INT NOT NULL,
+    ingredient_id INT FOREIGN KEY references ingredients(ingredient_id),
+    dish_id INT FOREIGN KEY references dishes(dish_id),
 	quantity NVARCHAR(50) NOT NULL,
-    PRIMARY KEY (ingredient_id, dish_id),
-    FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id),
-    FOREIGN KEY (dish_id) REFERENCES dishes(dish_id)
+    PRIMARY KEY (ingredient_id, dish_id)
 );
 
-CREATE TABLE user_ingredient (
-    ingredient_id INT NOT NULL,
-    user_id INT NOT NULL,
-    PRIMARY KEY (ingredient_id, user_id),
-    FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
+CREATE TABLE user_allergies (
+    user_id INT FOREIGN KEY references users(user_id),
+    ingredient_id INT FOREIGN KEY references ingredients(ingredient_id),
+    PRIMARY KEY (ingredient_id, user_id)
 );
+
+CREATE TABLE potluck_diet (
+	potluck_id INT FOREIGN KEY references potlucks(potluck_id),
+	diet_id INT FOREIGN KEY references diets(diet_id),
+	PRIMARY KEY (potluck_id, diet_id)
+);
+
+CREATE TABLE dish_diet (
+	dish_id INT FOREIGN KEY references dishes(dish_id),
+	diet_id INT FOREIGN KEY references diets(diet_id),
+	PRIMARY KEY (dish_id, diet_id)
+);
+
+CREATE TABLE diet_ingredient (
+	diet_id INT FOREIGN KEY references diets(diet_id),
+    ingredient_id INT FOREIGN KEY references ingredients(ingredient_id),
+	PRIMARY KEY (diet_id, ingredient_id)
+);
+
+CREATE TABLE user_diet (
+    user_id INT FOREIGN KEY references users(user_id),
+	diet_id INT FOREIGN KEY references diets(diet_id),
+	PRIMARY KEY (user_id, diet_id)
+);
+
+CREATE TABLE friends_list (
+	self_id INT FOREIGN KEY references users(user_id),
+	friend_id INT FOREIGN KEY references users(user_id),
+	friend_start DATETIME NULL,
+	PRIMARY KEY (self_id, friend_id)
+);
+
+CREATE TABLE dish_rating (
+	dish_id INT FOREIGN KEY references dishes(dish_id),
+	rater INT FOREIGN KEY references users(user_id),
+	rating INT NOT NULL
+	PRIMARY KEY (dish_id, rater)
+);
+
+CREATE TABLE dish_course (
+	dish_id INT FOREIGN KEY references dishes(dish_id),
+	course_id INT FOREIGN KEY references courses(course_id)
+);
+
 
 
 --populate default data
@@ -123,8 +167,11 @@ INSERT INTO users (username, password_hash, salt, user_role)
 VALUES ('kyle','Fjhd4G3BwUgJ4SYdtL1oWk8eFPo=', 'xWO2yPBd7Sg=','user');
 INSERT INTO users (username, password_hash, salt, user_role) 
 VALUES ('matt','XrITqHZWKG4kOj1U95ema/cbb20=', '8Vsd/EnDrOM=','user');
-INSERT INTO title (title_name)
+INSERT INTO titles (title_name)
 VALUES ('host'),('creator'),('guest');
+INSERT INTO diets (diet_name)
+VALUES ('Gluten Free'), ('Vegetarian'), ('Vegan'), ('Tree Nut Allergy'), ('Seafood Allergy'), ('Nut Allergy'), 
+	   ('Lactose Intolerant'), ('Soy Allergy');
 GO
 
 
