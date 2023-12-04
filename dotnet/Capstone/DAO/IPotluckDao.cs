e4310bd549074720145ebd8a976b97505255c4de
Matthew Altomare <mcaaltomare@gmail.com>
2023-12-04 13:43:22 -0500

    Updates to Potluck & Dish Models and DAOs.

--- dotnet/Capstone/DAO/IPotluckDao.cs
+++ dotnet/Capstone/DAO/IPotluckDao.cs
@@ -1,6 +1,15 @@
-﻿namespace Capstone.DAO
+﻿using Capstone.Models;
+using System.Collections.Generic;
+
+namespace Capstone.DAO
 {
     public interface IPotluckDao
     {
+        public IList<Potluck> GetPotlucks();
+        public Potluck GetPotluckById(int potluckId);
+        public Potluck GetPotluckByName(int potluckName);
+        public Potluck CreatePotluck(NewPotluckDTO incomingPotluck);
+        public Potluck UpdatePotluck(Potluck potluck, int hostId);
+
     }
 }
