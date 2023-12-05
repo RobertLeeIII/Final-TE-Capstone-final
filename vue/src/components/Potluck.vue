7975aacf9ec551f796b1b01fd11394f824660feb
Robert Lee <roblee8374@gmail.com>
2023-12-05 14:57:09 -0500

    added a rough potluck.vue comp

---
+++ vue/src/components/Potluck.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="potluck">
+      <h2>{{ potluck.name }}</h2>
+      <p>Date: {{ potluck.date }}</p>
+      <p>Theme: {{ potluck.theme }}</p>
+      <h3>Guests:</h3>
+      <ul>
+        <li v-for="Guests in potluck.Guests" :key="Guest.id">
+          {{ Guest.email }} 
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Potluck',
+    props: {
+      potluck: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .potluck {
+  }
+  </style>
+  
