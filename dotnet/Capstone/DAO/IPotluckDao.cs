870262d72e1ce27e8df9751db0505fa96e4b71ee
Matthew Altomare <mcaaltomare@gmail.com>
2023-12-04 20:55:18 -0500

    Implementation of IPotluckDao methods in PotluckSqlDao. Email property added to User model.

--- dotnet/Capstone/DAO/IPotluckDao.cs
+++ dotnet/Capstone/DAO/IPotluckDao.cs
@@ -8,8 +8,13 @@ namespace Capstone.DAO
         public IList<Potluck> GetPotlucks();
         public Potluck GetPotluckById(int potluckId);
         public Potluck GetPotluckByName(int potluckName);
+        public IList<Potluck> GetPotlucksByUserId(int userId);
         public Potluck CreatePotluck(NewPotluckDTO incomingPotluck);
-        public Potluck UpdatePotluck(Potluck potluck, int hostId);
+        public Potluck UpdatePotluck(UpdatePotluckDTO updatedPotluck, int potluckId);
+        public int DeletePotluck(int potluckId);
+        // TODO: Two separate SQL statments needed.
+        // DELETE FROM potluck_user WHERE potluck_user.user_id = @user_id AND potluck_user.potluck_id = @potluck_id
+        public int UninviteUser(int potluckId, int userId);
 
     }
 }
