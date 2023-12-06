8ab01b40b041bcf0a5418ce4831abbbd6b3f9239
Kelsey Brownwright <chaoticsunshine@gmail.com>
2023-12-06 12:10:59 -0500

    added dish controller

--- dotnet/Capstone/DAO/IDishDao.cs
+++ dotnet/Capstone/DAO/IDishDao.cs
@@ -7,8 +7,8 @@ namespace Capstone.DAO
     {
         public IList<Dish> GetDishes();
         public Dish GetDishById(int dishId);
-        public Dish GetDishByUserId(int userId);
-        public Dish GetDishByPotluckId(int potluckId);
+        public IList<Dish> GetDishesByUserId(int userId);
+        public IList<Dish> GetDishesByPotluckId(int potluckId);
         public Dish CreateNewDish(NewDishDTO newDish, int userId, int potluckId);
         public Dish UpdateDish(UpdateDishDTO updateDish, int dishId);
 
