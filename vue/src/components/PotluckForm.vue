936abcee19280d47443ec790d2ce0886235a5a62
Matthew Altomare <mcaaltomare@gmail.com>
2023-12-06 14:49:15 -0500

    Updates to the new potluck form in PotlucksForm.

--- vue/src/components/PotluckForm.vue
+++ vue/src/components/PotluckForm.vue
@@ -9,8 +9,23 @@
         <label for="location">Location</label>
         <input type="text" name="location" id="location" v-model="newPotluck.location">
 
-        <label for="time">Date</label>
-        <input type="datetime-local" name="time" id="time" v-model="newPotluck.date">
+        <label for="date">Date</label>
+        <input type="datetime-local" name="date" id="date" v-model="newPotluck.date">
+
+        <label for="courses">Courses</label>
+        <div>
+            <select id="course" v-model="newPotluck.course">
+                <option>--Select a Course--</option>
+                <option>Appetizers</option>
+                <option>Sides</option>
+                <option>Mains</option>
+                <option>Desserts</option>
+            </select>
+
+            <div id="courseCounterContainer" v-on:focus="showQuantityBox = true">
+                <input type="number" id="courseCounter" placeholder="0" v-show="newPotluck.course !== '--Select a Course--'" />
+            </div>
+        </div>
 
         <label for="potluck-theme">Theme</label>
         <select id="potluck-theme" v-model="newPotluck.theme">
@@ -29,16 +44,19 @@
 export default {
     data() {
         return {
-        newPotluck: {
-            name: '',
-            summary: '',
-            location: '',
-            date: '',
-            theme: 'None'
+
+            newPotluck: {
+                name: '',
+                summary: '',
+                location: '',
+                date: '',
+                course: '--Select a Course--',
+                theme: 'None'
+            },
+            showQuantityBox: false,
         }
     }
-    }
-    
+
 }
 </script>
 
