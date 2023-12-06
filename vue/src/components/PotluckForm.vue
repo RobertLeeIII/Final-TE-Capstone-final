2b719fcb3e5b934a5282518dd4b21818f4e8a335
Matthew Altomare <mcaaltomare@gmail.com>
2023-12-06 12:54:28 -0500

    Updates made to Potluck, PotluckForm, and UserDetailView.

--- vue/src/components/PotluckForm.vue
+++ vue/src/components/PotluckForm.vue
@@ -1,26 +1,45 @@
 <template>
-    <form>
+    <form v-on:submit.prevent="submitForm" class="potluck-form">
         <label for="potluck-name">Name</label>
-        <input type="text" name="Name" id="potluck-name">
+        <input type="text" name="Name" id="potluck-name" v-model="newPotluck.name">
 
         <label for="summary">Summary</label>
-        <input type="text" name="Summary" id="summary">
+        <input type="text" name="Summary" id="summary" v-model="newPotluck.summary">
 
         <label for="location">Location</label>
-        <input type="text" name="location" id="location">
+        <input type="text" name="location" id="location" v-model="newPotluck.location">
 
-        <label for="time">When</label>
-        <input type="datetime-local" name="time" id="time">
+        <label for="time">Date</label>
+        <input type="datetime-local" name="time" id="time" v-model="newPotluck.date">
 
         <label for="potluck-theme">Theme</label>
-        <input type="text" name="Theme" id="potluck-theme">
-
+        <select id="potluck-theme" v-model="newPotluck.theme">
+            <option>None</option>
+            <option>Spring</option>
+            <option>Summer</option>
+            <option>Fall</option>
+            <option>Winter</option>
+        </select>
+        <button type="submit">Submit</button>
 
     </form>
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+        newPotluck: {
+            name: '',
+            summary: '',
+            location: '',
+            date: '',
+            theme: 'None'
+        }
+    }
+    }
+    
+}
 </script>
 
-<style>
-</style>
+<style></style>
