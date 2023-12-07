ef246dc81559f74f040b7a7c2e2ec1f0f0187098
Robert Lee <roblee8374@gmail.com>
2023-12-07 10:24:32 -0500

    added styling to welcome screen

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,53 +1,55 @@
 <template>
   <div>
-    <section class="hero is-info">
-      <div class="hero-body">
-        <p class="title">
-          Welcome to the Potluck Planner
-        </p>
-        <p class="subtitle">
-          Plan and organize your potluck events!
-        </p>
+    <section class="layout">
+      <div class="header">
+        <div class="header-content">
+          <div class="header-text">
+            <h1 class="header-title">Pot·luck</h1>
+            <h2 class="header-phonetic">/ˌpätˈlək/</h2>
+            <h3 class="header-definition">noun</h3>
+            <p class="header-description">
+              Used in reference to a situation in which one must take a chance that whatever is available will prove to be
+              good or acceptable.
+            </p>
+            <p class="header-description">
+              <br>
+              Coordinate potluck dishes, supplies and party RSVPs with online sign up sheets for gatherings with family,
+              friends and large groups.
+
+            </p>
+          </div>
+          <div class="header-image">
+            <img src="/public/potluckHomeViewImage.jpg" alt="Image description" class="header-img">
+          </div>
+        </div>
+      </div>
+      <div class="main">
+        <h2>Your Potluck Events:</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pretium dolor sit amet est gravida, vitae
+          efficitur
+          ex mattis. Sed at ante ex. Vivamus ut ligula eu ex eleifend malesuada. Donec auctor sit amet neque venenatis
+          rutrum. Suspendisse consectetur dictum aliquam. Sed rutrum ullamcorper leo, ac venenatis tellus auctor in. Duis
+          finibus lobortis metus nec rhoncus. Ut metus quam, aliquam sit amet elit in, hendrerit facilisis nunc.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris tempor libero non
+          lacus fringilla, eu lobortis sapien viverra.</p>
+
+
+        <p>Nullam congue volutpat risus vel interdum. Vivamus non velit iaculis, feugiat tellus nec, gravida erat.
+          Aliquam id feugiat elit, in sagittis velit. Maecenas eget nibh eget sapien tincidunt euismod. Vivamus felis
+          eros,
+          vestibulum vitae fringilla nec, tincidunt eu enim. Morbi nec elementum orci. Cras lobortis, felis elementum
+          molestie convallis, lorem arcu cursus sapien, sed eleifend urna ligula sit amet nulla. Nullam blandit eleifend
+          libero. Curabitur convallis mattis tempor. Ut sed ante metus. Proin malesuada, felis non sollicitudin tempus,
+          orci lacus tristique nisi, imperdiet aliquam nisl libero at ante. Nullam at justo lobortis, imperdiet diam in,
+          maximus erat.</p>
+      </div>
+      <div class="centerFooter">
+        <p><strong>potluckPlanner:</strong> Written by TE capstone team 2</p>
       </div>
     </section>
-
-    <div class="container">
-      <h2>Your Potluck Events:</h2>
-      <!-- Display potluck events or create new ones -->
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pretium dolor sit amet est gravida, vitae efficitur
-        ex mattis. Sed at ante ex. Vivamus ut ligula eu ex eleifend malesuada. Donec auctor sit amet neque venenatis
-        rutrum. Suspendisse consectetur dictum aliquam. Sed rutrum ullamcorper leo, ac venenatis tellus auctor in. Duis
-        finibus lobortis metus nec rhoncus. Ut metus quam, aliquam sit amet elit in, hendrerit facilisis nunc. Vestibulum
-        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris tempor libero non lacus
-        fringilla, eu lobortis sapien viverra.
-
-        Aenean vehicula congue augue quis semper. Nullam hendrerit augue in odio egestas maximus. Praesent placerat
-        facilisis ullamcorper. Donec massa ex, pulvinar sit amet accumsan id, porta ut augue. In ultrices orci in purus
-        dignissim aliquam. Aenean vel pharetra sem, vitae scelerisque mauris. Sed lacinia, lectus placerat tempor dictum,
-        ex mi mollis urna, non porta nisi nunc a mi. Praesent lobortis mauris eget faucibus mattis. Quisque vestibulum
-        tristique dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-        Nullam congue volutpat risus vel interdum. Vivamus non velit iaculis, feugiat tellus nec, gravida erat. Aliquam id
-        feugiat elit, in sagittis velit. Maecenas eget nibh eget sapien tincidunt euismod. Vivamus felis eros, vestibulum
-        vitae fringilla nec, tincidunt eu enim. Morbi nec elementum orci. Cras lobortis, felis elementum molestie
-        convallis, lorem arcu cursus sapien, sed eleifend urna ligula sit amet nulla. Nullam blandit eleifend libero.
-        Curabitur convallis mattis tempor. Ut sed ante metus. Proin malesuada, felis non sollicitudin tempus, orci lacus
-        tristique nisi, imperdiet aliquam nisl libero at ante. Nullam at justo lobortis, imperdiet diam in, maximus erat.
-
-        Sed tristique ut elit et malesuada. Aliquam ornare tristique nunc, a venenatis magna mollis quis. Ut ornare
-        interdum posuere. Suspendisse scelerisque odio sem, non pharetra justo efficitur id. Donec malesuada imperdiet
-        elit, id laoreet erat pellentesque eu. Suspendisse potenti. Nulla non sapien tristique, faucibus nisl vitae,
-        fringilla orci.
-
-        Suspendisse auctor nulla sed lectus tempus ornare pulvinar non magna. Cras at blandit nisl. Morbi vel ipsum felis.
-        Donec elementum velit in libero ultricies fringilla. Nullam fringilla velit non dolor porta, at consequat libero
-        rutrum. Morbi blandit, leo laoreet accumsan interdum, sapien dolor venenatis tortor, eu iaculis eros est in mi.
-        Sed malesuada congue ante, dapibus pretium augue fringilla sed. Proin vestibulum elit id lacus posuere, pharetra
-        imperdiet enim gravida. Sed vulputate vulputate nulla et semper. Vivamus scelerisque ipsum ut tincidunt accumsan.
-        Cras mollis rhoncus faucibus.</p>
-    </div>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -72,4 +74,85 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.layout {
+  display: grid;
+  grid-template-rows: auto 1fr auto; /* Adjust as needed */
+  gap: 8px;
+  background-color: salmon; /* Salmon-colored background */
+  min-height: 95vh; /* Set minimum height of layout to full viewport height */
+
+  padding-bottom: 20px; /* Increase bottom padding */
+}
+
+.centerFooter {
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+  margin-top: 20px; /* Move the footer down */
+  color: white; /* Change text color */
+}
+
+.header {
+  text-align: left;
+  margin-bottom: 20px;
+  background-color: white; /* Set background color for the header */
+}
+
+.header-content {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  border: 4px solid salmon;
+  padding: 20px; /* Add padding as needed */
+}
+
+.header-text {
+  text-align: left;
+
+  margin-right: 50px;
+  margin-left: 50px;
+}
+
+.header-title {
+  font-size: 36px;
+  margin-bottom: 5px;
+}
+
+.header-phonetic {
+  font-size: 18px;
+  margin-left: 50px;
+
+  color: #888;
+}
+
+.header-definition {
+  font-size: 24px;
+  margin-top: 10px;
+  margin-bottom: 5px;
+  margin-left: 50px;
+
+}
+
+.header-description {
+  font-size: 16px;
+
+  margin-left: 50px;
+  line-height: 1.5;
+}
+
+.header-image {
+  flex-shrink: 0;
+}
+
+.header-img {
+  width: 380px;
+  height: auto;
+  border-radius: 4%;
+}
+
+.main{
+  color: white; /* Change text color */
+
+}</style>
