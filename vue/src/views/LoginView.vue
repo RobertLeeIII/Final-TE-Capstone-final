16824124596bbe794be81346d4c392f7b5fdf28a
Robert Lee <roblee8374@gmail.com>
2023-12-07 16:14:04 -0500

    fixed merge again

--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -36,12 +36,6 @@
         </div>
       </div>
     </div>
-
-      <button class="button is-primary" type="submit">Sign in</button>
-      <p>
-        <br>
-    <router-link v-bind:to="{ name: 'register' }" class="button is-link" >Need an account? Sign up.</router-link>
-  </p>
   <p class="forgot-password text-right">
     <router-link to="forgot">Forgot Password?</router-link>
   </p>
