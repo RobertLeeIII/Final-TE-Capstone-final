f2fa887c69ba011ddb04bfb741a4d1c52015d736
Matthew Altomare <mcaaltomare@gmail.com>
2023-12-08 10:51:06 -0500

    conditional themes on the right track in PotluckDetail.vue.

--- vue/src/components/PotluckDetail.vue
+++ vue/src/components/PotluckDetail.vue
@@ -1,18 +1,24 @@
 <template>
-    <div class="potluck-detail">
-        <h2>{{ Potluck.name }}</h2>
-        <h2>{{ Potluck.location }}</h2>
-        <h2>{{ Potluck.time }}</h2>
-        <h2>{{ Potluck.theme }}</h2>
-        <h2>{{ Potluck.summary }}</h2>
-        <h2>Appetizers: {{ Potluck.courseRequest.apps }}</h2>
-        <h2>Main Dishes: {{ Potluck.courseRequest.mains }}</h2>
-        <h2>Sides: {{ Potluck.courseRequest.sides }}</h2>
-        <h2>Desserts: {{ Potluck.courseRequest.desserts }}</h2>
-        <router-link v-if="isHost" :to="{name: invitationList}">Invite People</router-link>
-        <router-link v-if="isHost" :to="{name: 'potluck-update', params: {potluckId: Potluck.potluckId}, 
-                                        query: {action: 'update'}}">Update This Potluck</router-link>
-    </div>
+    <body>
+        <div :class="changingTheme">
+            <ul>
+            <li><i class="fa-regular fa-snowflake"></i>{{ Potluck.name }}</li>
+            <li><i class="fa-regular fa-snowflake"></i>{{ Potluck.location }}</li>
+            <li><i class="fa-regular fa-snowflake"></i>{{ Potluck.time }}</li>
+            <li><i class="fa-regular fa-snowflake"></i>{{ Potluck.theme }}</li>
+            <li><i class="fa-regular fa-snowflake"></i>{{ Potluck.summary }}</li>
+            <li><i class="fa-regular fa-snowflake"></i>Appetizers: {{ Potluck.courseRequest.apps }}</li>
+            <li><i class="fa-regular fa-snowflake"></i>Main Dishes: {{ Potluck.courseRequest.mains }}</li>
+            <li><i class="fa-regular fa-snowflake"></i>Sides: {{ Potluck.courseRequest.sides }}</li>
+            <li><i class="fa-regular fa-snowflake"></i>Desserts: {{ Potluck.courseRequest.desserts }}</li>
+        </ul>
+            <router-link v-if="isHost" :to="{ name: invitationList }">Invite People</router-link>
+            <router-link v-if="isHost" :to="{
+                name: 'potluck-update', params: { potluckId: Potluck.potluckId },
+                query: { action: 'update' }
+            }">Update This Potluck</router-link>
+        </div>
+    </body>
 </template>
 
 <script>
@@ -20,11 +26,31 @@ export default {
     data() {
         return {
             //currentPotluck: {}
+            themeSpring: false,
+            themeSummer: false,
+            themeFall: false,
+            themeWinter: false,
         }
     },
     computed: {
         isHost() {
             return this.Potluck.hostId == this.$store.state.user.userId;
+        },
+        changingTheme() {
+            if (this.Potluck.theme === 'Spring') {
+                this.themeSpring === true;
+            }
+            else if (this.Potluck.theme === 'Summer') {
+                this.themeSummer === true;
+            }
+            else if (this.Potluck.theme === 'Fall') {
+                this.themeFall === true;
+            }
+            else if (this.Potluck.theme === 'Winter') {
+                return {winter: true}
+                //this.themeWinter === true;
+            }
+            return true;
         }
     },
     props: {
@@ -34,8 +60,24 @@ export default {
 </script>
 
 <style scoped>
-.potluck-detail h2{
+.potluck-detail h2 {
     padding: 5px;
 
 }
-</style>
+
+.spring {
+    color: red;
+}
+
+.summer {
+    color: limegreen;
+}
+
+.fall {
+    color: blue;
+}
+
+.winter {
+    color: cadetblue;
+    background-color: purple;
+}</style>
