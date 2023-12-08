6224596783480ee9349417d47670c817cad415d2
Kyle Wacker <kylewwacker@gmail.com>
2023-12-08 16:01:41 -0500

    added a password reset controller and some changes to my vue code for reset and forgot

---
+++ dotnet/Capstone/Controllers/PasswordResetController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Linq;
+
+[ApiController]
+[Route("controller")]
+public class PasswordResetController : ControllerBase
+{
+    private const string JwtSecret = "your-secret-key";
+    private const string EmailSender = "plannerpotluck@gmail.com";
+    private const string EmailSenderPassword = "C#C#DotNet";
+
+    [HttpPost("forgot")]
+    public IActionResult ForgotPassword([FromBody] ForgotPasswordRequest request)
+    {
+        if (string.IsNullOrEmpty(request?.Email))
+        {
+            return BadRequest("Email is required");
+        }
+
+        var token = GenerateJwtToken(request.Email);
+        var emailSent = SendResetEmail(request.Email, token);
+
+        if (emailSent)
+        {
+            return Ok(new { Token = token, Message = "Email sent successfully" });
+        }
+
+        return StatusCode((int)HttpStatusCode.InternalServerError, new { Error = "Error sending email" });
+    }
+
+    [HttpPost("reset")]
+    public IActionResult ResetPassword([FromBody] ResetPasswordRequest request)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(request?.Token) || string.IsNullOrEmpty(request?.Password) || string.IsNullOrEmpty(request?.PasswordConfirm))
+            {
+                return BadRequest("Token, Password, and PasswordConfirm are required");
+            }
+
+            var email = ValidateJwtToken(request.Token);
+
+            if (email == null)
+            {
+                return Unauthorized("Invalid token");
+            }
+
+            // TODO: Implement password reset logic here (update the password in your database, for example)
+
+            return Ok(new { Message = "Password reset successfully" });
+        }
+        catch (SecurityTokenExpiredException)
+        {
+            return BadRequest("Token has expired");
+        }
+        catch (Exception)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, new { Error = "Error resetting password" });
+        }
+    }
+
+    private string GenerateJwtToken(string email)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = System.Text.Encoding.ASCII.GetBytes(JwtSecret);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[] { new Claim("email", email) }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    private string ValidateJwtToken(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = System.Text.Encoding.ASCII.GetBytes(JwtSecret);
+
+        tokenHandler.ValidateToken(token, new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        }, out var validatedToken);
+
+        var jwtToken = (JwtSecurityToken)validatedToken;
+        return jwtToken?.Claims?.FirstOrDefault(x => x.Type == "email")?.Value;
+    }
+
+    private bool SendResetEmail(string email, string token)
+    {
+        try
+        {
+            var fromAddress = new MailAddress(EmailSender, "Your Name");
+            var toAddress = new MailAddress(email, "User Name");
+            const string subject = "Password Reset";
+            var body = $"Click the following link to reset your password: http://localhost:8080/reset/{token}";
+
+            using (var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com", // Update with your SMTP server
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, EmailSenderPassword)
+            })
+            {
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+                {
+                    smtp.Send(message);
+                    return true;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            // Log or handle the exception
+            return false;
+        }
+    }
+}
+
+public class ForgotPasswordRequest
+{
+    public string Email { get; set; }
+}
+
+public class ResetPasswordRequest
+{
+    public string Token { get; set; }
+    public string Password { get; set; }
+    public string PasswordConfirm { get; set; }
+}
--- vue/src/components/Forgot.vue
+++ vue/src/components/Forgot.vue
@@ -1,46 +1,61 @@
-<template @submit.prevent="handleSubmit">
-    <form class="box">
-    <h3>Forgot Password?</h3>
-    <br>
-    <div class="field">
-        <label>Email</label>
-        <div class='control'>
-        <input type="email" class="form-control" v-model="email" placeholder="Email">
+<template>
+    <div>
+      <form class="box" @submit.prevent="handleSubmit">
+        <h3>Forgot Password?</h3>
+        <br />
+        <div class="field">
+          <label>Email</label>
+          <div class="control">
+            <input type="email" class="form-control" v-model="email" placeholder="Email" />
+          </div>
         </div>
+  
+        <button class="button is-primary">Submit</button>
+      </form>
+      <div class="forimage">
+        <img class="img-size" src="/public\wall.jpg" />
+      </div>
     </div>
-
-    <button class="button is-primary">Submit</button>
-    </form>
-    <div class="forimage">
-    <img class='img-size' src="/public\wall.jpg">
-    </div>
-</template>
-
-<script>
-import axios from 'axios';
-export default{
-    name:'Forgot',
-    data(){
-        return{
-          email: ''  
-        }
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'Forgot',
+    data() {
+      return {
+        email: ''
+      };
     },
     methods: {
-        async handleSubmit(){
-            const response = await axios.post('forgot', {
-                    email: this.email
-            });
-            /* console.log(response); */
+      async handleSubmit() {
+        try {
+          // Make an API request to send the reset link to the email
+          const response = await axios.post('/api/forgot-password', {
+            email: this.email
+          });
+  
+          console.log(response.data);
+          // Assuming your backend returns a success message
+  
+          // Redirect the user to the reset route with a token
+          this.$router.push(`/reset/${response.data.token}`);
+        } catch (error) {
+          console.error(error.response.data);
+          // Handle errors appropriately
         }
+      }
     }
-}
-</script>
-<style>
-.img-size{
-    height: 650px; 
-}
-.forimage{
+  };
+  </script>
+  
+  <style>
+  .img-size {
+    height: 650px;
+  }
+  .forimage {
     display: flex;
     justify-content: center;
-}
-</style>
+  }
+  </style>
