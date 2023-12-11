189c252557a221493074912952e801a7680e7239
Kyle Wacker <kylewwacker@gmail.com>
2023-12-11 12:53:41 -0500

    api styling

--- vue/src/components/ApiTest.vue
+++ vue/src/components/ApiTest.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
-    <button @click="getRecipe" class="button">Recipe Suggestion</button>
-    <br><br>
-    <div v-if="currentRecipe" class="recipe-container">
-      <h2 class="recipe-title">{{ currentRecipe.strMeal }}</h2>
-      <img :src="currentRecipe.strMealThumb" :alt="currentRecipe.strMeal" class="recipe-image" />
-      <p class="recipe-instructions">{{ currentRecipe.strInstructions }}</p>
+    <div class="page-container">
+      <button @click="getRecipe" class="button">Recipe Suggestion</button>
+      <br><br>
+      <div v-if="currentRecipe" class="recipe-container">
+        <h2 class="recipe-title">{{ currentRecipe.strMeal }}</h2>
+        <img :src="currentRecipe.strMealThumb" :alt="currentRecipe.strMeal" class="recipe-image" />
+        <p class="recipe-instructions">{{ currentRecipe.strInstructions }}</p>
+      </div>
+      <br>
+      <div v-if="showIngredients" class="ingredients">List of Ingredients:</div>
+      <br>
+      <ul v-if="showIngredients" class="ingredient-list">
+        <li v-for="(ingredient, index) in getIngredients()" :key="index" class="ingredient-item">
+          {{ ingredient }}
+        </li>
+      </ul>
     </div>
-    <br>
-    <div v-if="showIngredients" class="ingredients">List of Ingredients:</div>
-    <br>
-    <ul v-if="showIngredients" class="ingredient-list">
-      <li v-for="(ingredient, index) in getIngredients()" :key="index" class="ingredient-item">
-        {{ ingredient }}
-      </li>
-    </ul>
   </div>
 </template>
 
@@ -70,6 +72,11 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #cef0c3;
+  margin: 0;
+}
+
 .button {
   background-color: #4CAF50;
   border: none;
@@ -83,29 +90,42 @@ export default {
   cursor: pointer;
 }
 
+.page-container {
+  background-color: #cef0c3;
+  padding: 20px; /* Add padding as needed */
+}
+
 .recipe-container {
   text-align: center;
+  padding: 15px; /* Add padding as needed */
+  margin-top: 20px; /* Adjust margin as needed */
 }
 
 .recipe-title {
   font-size: 24px;
   margin-bottom: 10px;
+  border-bottom: 2px solid #4CAF50; /* Add a line below the recipe title */
+  padding-bottom: 10px; /* Add padding as needed */
 }
 
 .recipe-image {
   max-width: 100%;
   border-radius: 8px;
-  margin-bottom: 20px;
+  margin: 20px 0; /* Adjust margin as needed */
 }
 
 .recipe-instructions {
   font-size: 16px;
   margin-bottom: 20px;
+  border-bottom: 2px solid #4CAF50; /* Add a line below the instructions */
+  padding-bottom: 10px; /* Add padding as needed */
 }
 
 .ingredients {
   font-size: 18px;
   font-weight: bold;
+  border-bottom: 2px solid #4CAF50; /* Add a line below the ingredients title */
+  padding-bottom: 10px; /* Add padding as needed */
 }
 
 .ingredient-list {
