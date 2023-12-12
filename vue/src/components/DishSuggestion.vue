4f363b473a1c93844c56b460f430f5152305375e
Robert Lee <roblee8374@gmail.com>
2023-12-12 14:41:16 -0500

    CheatGPT to change vue version

--- vue/src/components/DishSuggestion.vue
+++ vue/src/components/DishSuggestion.vue
@@ -21,92 +21,87 @@
   </template>
   
   <script>
-  import { ref, computed, onMounted } from 'vue';
   import MealDBService from '@/services/MealDBService.js';
   
   export default {
     props: ['course'],
-    setup(props) {
-      const mealSuggestions = ref([]);
-      const currentCourse = ref(0);
-  
-      const courseName = computed(() => {
-        return props.course === 1 ? 'appetizer' : props.course === 2 ? 'side' : props.course === 3 ? 'main dish' : 'dessert';
-      });
-  
-      const shuffledMeals = computed(() => {
-        const mealsCopy = mealSuggestions.value.slice(); // Create a copy of the array
+    data() {
+      return {
+        mealSuggestions: [],
+        currentCourse: 0
+      };
+    },
+    computed: {
+      courseName() {
+        return this.course === 1 ? 'appetizer' : this.course === 2 ? 'side' : this.course === 3 ? 'main dish' : 'dessert';
+      },
+      shuffledMeals() {
+        const mealsCopy = this.mealSuggestions.slice(); // Create a copy of the array
         return mealsCopy.sort(() => Math.random() - 0.5);
-      });
-  
-      const getNewDish = async () => {
-        mealSuggestions.value = [];
+      }
+    },
+    methods: {
+      async getNewDish() {
+        this.mealSuggestions = [];
         try {
-          const response = await MealDBService.getRecipeByCourseId(currentCourse.value);
-          mealSuggestions.value = response.data.meals;
+          const response = await MealDBService.getRecipeByCourseId(this.currentCourse);
+          this.mealSuggestions = response.data.meals;
         } catch (error) {
           console.error('Error fetching meal suggestions:', error);
         }
-      };
-  
-      onMounted(() => {
-        currentCourse.value = props.course;
-        getNewDish(); // Fetch initial meal suggestions on component creation
-      });
-  
-      return {
-        currentCourse,
-        courseName,
-        shuffledMeals,
-        getNewDish
-      };
+      }
+    },
+    mounted() {
+      this.currentCourse = this.course;
+      this.getNewDish(); // Fetch initial meal suggestions on component creation
     }
   };
   </script>
   
   <style scoped>
-.signup-section {
-  text-align: center;
-  padding: 20px;
-}
-
-.signup-button,
-.new-dish-button {
-  padding: 10px 20px;
-  margin: 10px;
-  background-color: #3498db;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-}
-
-.signup-button:hover,
-.new-dish-button:hover {
-  background-color: #2980b9;
-}
-
-.grid-container {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-template-rows: auto auto;  
-  gap: 20px;
-  grid-auto-rows: 0px;
-  justify-items: center;
-  max-height: 750px;
-  overflow: hidden;
-
-}
-
-.meal-item {
-  margin: 0;
-}
-
-.meal-image {
-  width: 100%;
-  height: auto;
-  border-radius: 5px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-</style>
+  .signup-section {
+    text-align: center;
+    padding: 20px;
+  }
+  
+  .signup-button,
+  .new-dish-button {
+    padding: 10px 20px;
+    margin: 10px;
+    background-color: #3498db;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+  
+  .signup-button:hover,
+  .new-dish-button:hover {
+    background-color: #2980b9;
+  }
+  
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-template-rows: auto auto;  
+    gap: 20px;
+    grid-auto-rows: 0px;
+    justify-items: center;
+    max-height: 750px;
+    overflow: hidden;
+  
+  }
+  
+  .meal-item {
+    margin: 0;
+  }
+  
+  .meal-image {
+    width: 100%;
+    height: auto;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+  </style>
+  
