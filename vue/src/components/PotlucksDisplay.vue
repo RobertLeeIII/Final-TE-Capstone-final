da35ab17db314f3e25a31479350400ea8fce867f
Kelsey Brownwright <chaoticsunshine@gmail.com>
2023-12-14 18:29:49 -0500

    constrained inputs on potluck form, got rid of sad loner no-potlucks form

--- vue/src/components/PotlucksDisplay.vue
+++ vue/src/components/PotlucksDisplay.vue
@@ -25,24 +25,7 @@
             </potluck>
             <potluck class="potluck future" v-for="potluck in futurePotlucks" :propPotluck="potluck" :key="potluck.potluckId">
             </potluck>
-            <div class="card-container" v-if="!hasPotlucks">
-                <div class="card">
-                    <div class="card-image">
-                        <figure class="image is-centered">
-                            <img src="/public/imagePotluck-transformed.jpg" alt="Potluck Image">
-                        </figure>
-                    </div>
-                    <div class="card-content">
-
-                        <div class="media">
-                            <div class="media-center">
-                                <p>You Have No Potlucks :(</p>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+           
 
         </div>
     </div>
